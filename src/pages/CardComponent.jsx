9d28c8558baf2291f299fa3c8dbe98d0cea04d4e
Rafael <rafaelaraujo.contact@gmail.com>
2022-10-03 22:28:12 -0300

    Kelly e Emerson

--- src/pages/CardComponent.jsx
+++ src/pages/CardComponent.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
-export default function CardComponent(){
+export default function CardComponent() {
+  const [dog, setDog] = useState([])
+  const { name } = useParams()
+
+  useEffect(() => {
+    const fetchUnicoDogData = async () => {
+      try {
+        const res = await fetch(
+          `https://api.thedogapi.com/v1/breeds/search?q=${name}`
+        )
+        const data = await res.json()
+        setDog(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchUnicoDogData()
+  }, [name])
   return (
-    <div>Card Dog</div>
+    <>
+      <div className="container d-flex justify-content-center align-items-center vh-100 ">
+        {dog.map(item => (
+          <div key={item.id}>
+            <div className="row ">
+              <div className="col">
+                <img
+                  src={`https://cdn2.thedogapi.com/images/${item.reference_image_id}.jpg`}
+                  alt={item.name}
+                  className="rounded img-fluid"
+                />
+              </div>
+              <div className="col">
+                <h2 className="fw-bold">{item.name}</h2>
+                <ul>
+                  <li>Criado para: {item.bred_for}</li>
+                  <li>Altura: {item.height.metric} cm</li>
+                  <li>Expectativa de vida: {item.life_span}</li>
+                  <li>Temperamento: {item.temperament}</li>
+                </ul>
+                <Link to={'/'} className="mt-5 inline-block btn btn-primary btn-dark">
+                  Voltar
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,41 @@ import { Link } from 'react-router-dom'
 
 export default function home() {
   const [dogs, setDogs] = useState([])
+  const [text, setText] = useState('')
+  const [searched, setSearched] = useState(false)
+
   useEffect(() => {
     const fetchDogData = async () => {
       try {
         const res = await fetch('https://api.thedogapi.com/v1/breeds')
         const data = await res.json()
         setDogs(data)
-        console.log(data)
       } catch (error) {
         console.error(error)
       }
     }
+
+    setSearched(false)
     fetchDogData()
   }, [])
+
+  const searchForDog = async () => {
+    try {
+      const res = await fetch(
+        `https://api.thedogapi.com/v1/breeds/search?q=${text}`
+      )
+      const data = await res.json()
+      setDogs(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    searchForDog()
+    setSearched(true)
+  }
 
   return (
     <>
@@ -30,9 +52,14 @@ export default function home() {
           </section>
 
           <div className="container mt-3">
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicSearch">
-                <Form.Control type="text" placeholder="Labrador" />
+                <Form.Control
+                  type="text"
+                  placeholder="German"
+                  value={text}
+                  onChange={e => setText(e.target.value)}
+                />
                 <Form.Text className="text-muted">
                   Encontre qualquer raça de cachorro.
                 </Form.Text>
@@ -40,24 +67,49 @@ export default function home() {
             </Form>
           </div>
 
-          <div className="container">
+          <div className="container ">
             <div className="row cols d-flex justify-content-center gap-5">
-              {dogs.map(dog => (
-                <Card style={{ width: '18rem' }} className="p-0">
-                  <Card.Img
-                    className="h-50 d-inline-block"
-                    variant="top"
-                    src={dog.image.url}
-                    alt={dog.name}
-                  />
-                  <Link to={`/${dog.name}`} style={{textDecoration: 'none', color: 'black'}} key={dog.id}>
-                    <Card.Body>
-                      <Card.Title>{dog.name}</Card.Title>
-                      <Card.Text>Criado para: {dog.bred_for}</Card.Text>
-                    </Card.Body>
-                  </Link>
-                </Card>
-              ))}
+              {!searched
+                ? dogs.map(dog => (
+                    <Card style={{ width: '18rem' }} className="p-0">
+                      <Card.Img
+                        className="img-responsive h-50"
+                        variant="top"
+                        src={dog.image.url}
+                        alt={dog.name}
+                      />
+                      <Link
+                        to={`/${dog.name}`}
+                        style={{ textDecoration: 'none', color: 'black' }}
+                        key={dog.id}
+                      >
+                        <Card.Body>
+                          <Card.Title>{dog.name}</Card.Title>
+                          <Card.Text>Criado para: {dog.bred_for}</Card.Text>
+                        </Card.Body>
+                      </Link>
+                    </Card>
+                  ))
+                : dogs.map(dog => (
+                    <Card style={{ width: '18rem' }} className="p-0">
+                      <Card.Img
+                        className="img-responsive h-50"
+                        variant="top"
+                        src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}
+                        alt={dog.name}
+                      />
+                      <Link
+                        to={`/${dog.name}`}
+                        style={{ textDecoration: 'none', color: 'black' }}
+                        key={dog.id}
+                      >
+                        <Card.Body>
+                          <Card.Title>{dog.name}</Card.Title>
+                          <Card.Text>Criado para: {dog.bred_for}</Card.Text>
+                        </Card.Body>
+                      </Link>
+                    </Card>
+                  ))}
             </div>
           </div>
         </>
